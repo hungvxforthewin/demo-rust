26063d7ad7f34cd968d9014f0db9bb0d7ecd866d
hungvx <vuxuanhungmta@gmail.com>
2022-06-20 17:36:01 +0700

    loop break

--- 3A.CommonProgrammingConcepts/ControlFlow/src/main.rs
+++ 3A.CommonProgrammingConcepts/ControlFlow/src/main.rs
@@ -26,4 +26,31 @@ fn main() {
     // //  let number = if condition { 5 } else { "six" };
 
     // println!("The value of number is: {}", number);
+
+
+    // loop {
+    //     println!("again!");
+    // }
+
+
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {}", count);
+    //     let mut remaining = 10;
+
+    //     loop {
+    //         println!("remaining = {}", remaining);
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -= 1;
+    //     }
+
+    //     count += 1;
+    // }
+    // println!("End count = {}", count);
+
 }
