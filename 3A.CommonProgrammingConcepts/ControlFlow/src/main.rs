605190b5cb0c3018586ac8658352aedc7ecbecb7
hungvx <vuxuanhungmta@gmail.com>
2022-06-21 00:12:39 +0700

    loop in rust

--- 3A.CommonProgrammingConcepts/ControlFlow/src/main.rs
+++ 3A.CommonProgrammingConcepts/ControlFlow/src/main.rs
@@ -53,4 +53,46 @@ fn main() {
     // }
     // println!("End count = {}", count);
 
+    /*
+        Returning Values from Loops
+     */
+
+    //  let mut counter = 0;
+    //  let result = loop {
+    //     counter += 1;
+
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    //  };
+
+    // println!("The result is {}", result);
+
+    /*
+        Conditional Loops with while
+    */
+
+    // let mut number: i32 = 3;
+
+    // while number != 0 {
+    //     println!("{}!", number);
+
+    //     number -= 1;
+    // }
+
+    // println!("LIFTOFF!!!");
+
+    let my_array = [7, 8, 3, 9, 9];
+    let mut index: usize = 0; // why i32
+
+    while index < 5 {
+        println!("the value is: {}", my_array[index]);
+        index += 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
 }
