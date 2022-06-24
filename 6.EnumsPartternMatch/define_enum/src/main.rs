aabcfc495acb12c3b0e406ae7a19146fd3d39aae
hungvx <vuxuanhungmta@gmail.com>
2022-06-24 15:05:36 +0700

    enum option

--- 6.EnumsPartternMatch/define_enum/src/main.rs
+++ 6.EnumsPartternMatch/define_enum/src/main.rs
@@ -10,6 +10,14 @@ enum IpAddr_2 {
     V6(String),
 }
 
+// #[derive(Debug)]
+// enum Option<T> {
+    
+//     Some(T), // used to return a value
+//     None     // used to return null, as Rust doesn't support
+//              // the null keyword
+// }
+
 fn route(ip_kind: IpAddrKind) {}
 
 struct IpAddr {
@@ -18,13 +26,20 @@ struct IpAddr {
 }
 
 fn main() {
+
+    /*
+        Illustration: Enum
+    */
+
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
 
-
+    /*
+        Struct and Enum
+    */
     let home = IpAddr {
         kind: IpAddrKind::V4,
         address: String::from("127.0.0.1"),
@@ -41,4 +56,18 @@ fn main() {
 
     println!("{:?}, {:?}", home, loopback);
     println!("{:?}, {:?}", four, six);
+
+    /*
+        Enum option
+    */ 
+    let result = is_even(3);
+    println!("{:?}", result);
+    println!("{:?}", is_even(30));
+}
+fn is_even(no: i32) -> Option<bool> {
+    if no%2 == 0 {
+       Some(true)
+    } else {
+       None
+    }
 }
